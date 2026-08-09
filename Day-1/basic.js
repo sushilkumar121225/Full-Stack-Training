3a865fb8750f4cbc258afb57510419eba1047df3
Sushil Kumar <sushilkumarnk25102@gmail.com>
2026-08-09 17:24:42 +0530

    Add files via upload

---
+++ Day-1/basic.js
@@ -0,0 +1,16 @@
+// 1. Basic
+// console.log("Hello, World!");
+
+// // 2. Variables
+// var name = "John";      
+// let age = 25;           
+// const city = "NYC"; 
+// console.log(name, age, city);    
+
+// 3. Operators
+let sum = 10 + 5;       
+let product = 10 * 5;   
+let comparison = 10 > 5; 
+console.log(sum, product, comparison);
+
+
